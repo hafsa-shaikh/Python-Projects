e6a4571c7c5eff8c46245b0cc372b185bcd82ffa
Hafsa Shaikh <your-email@example.com>
2025-03-18 17:59:44 -1200

    python projects

---
+++ madlibs.py
@@ -0,0 +1,11 @@
+place = input("Place: ")
+stage = input("Stage: ")
+verb1 = input("Verb: ")
+animal1 = input("Animal: ")
+animal2 = input("Animal: ")
+adj = input("Adjective: ")
+
+# place = beach, stage = child, verb1 = spend, animal1 = camel, animal2 = horse, adjective = happening.
+madlib = f"Yesterday we went to the {place}. I use to go there since i was a {stage} with my family, once in a weeek. We use to {verb1} a quality time together. we take rides of {animal1} and {animal2} then we do lunch and get back to home. It's a refresher for us after a {adj} month!"
+print(madlib)
+ 
